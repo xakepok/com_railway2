89747aa5e02094b080feaf861b0a823f5cdc9f39
Anton <anton@nazvezde.ru>
2017-01-30 15:33:06 +0300

    Вынос фильтра на станциях в отдельный файл

--- component/administrator/views/stations/tmpl/default.php
+++ component/administrator/views/stations/tmpl/default.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * @package    railway2
- *
- * @author     Admin <your@email.com>
- * @copyright  A copyright
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * @link       http://your.url.com
- */
-
 defined('_JEXEC') or die;
 JHtml::_('behavior.tooltip');
 
@@ -18,12 +9,7 @@ JHtml::_('behavior.tooltip');
 <div id="j-main-container" class="span10">
     <form action="<?php echo JRoute::_('index.php?option=com_railway2&view=stations'); ?>" method="post" name="adminForm" id="adminForm">
         <fieldset id="filter-bar">
-            <div>
-                <label for="filter_search"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?></label>
-                <input type="text" autocomplete="off" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_RAILWAY2_SEARCH_STATION'); ?>" />
-                <button type="submit"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
-                <button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
-            </div>
+            <?=$this->loadTemplate('filter')?>
         </fieldset>
         <div><br><br></div>
         <table class="adminlist">
