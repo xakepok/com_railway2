c9fd531ffed6785824192d91314e416253cabe74
xakepok <xakepok@xakepok.com>
2017-10-13 18:48:53 +0300

    Меню

--- component/site/views/railway2/tmpl/default.php
+++ component/site/views/railway2/tmpl/default.php
@@ -7,5 +7,11 @@ defined('_JEXEC') or die;
 		<li>
 			<?php echo JHtml::link(JRoute::_('index.php?option=com_railway2&view=directions&Itemid=249'), JText::_('COM_RAILWAY2_DIRECTIONS'));?>
 		</li>
+		<li>
+			<?php echo JHtml::link(JRoute::_('index.php?option=com_content&view=article&id=62&Itemid=250'), 'Самые часто задаваемые вопросы');?>
+		</li>
+		<li>
+			<?php echo JHtml::link(JRoute::_('index.php?option=com_content&view=category&layout=blog&id=66&Itemid=251'), 'Терминология');?>
+		</li>
 	</ul>
 </div>
