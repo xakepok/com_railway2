ac99c2c44f6c1722fdda90ad3d5924afee9e7348
xakepok <xakepok@xakepok.com>
2017-11-23 11:43:14 +0300

    Меню справочника

--- component/site/views/railway2/tmpl/default.php
+++ component/site/views/railway2/tmpl/default.php
@@ -4,14 +4,17 @@ defined('_JEXEC') or die;
 
 <div>
 	<ul>
-		<li>
-			<?php echo JHtml::link(JRoute::_('index.php?option=com_railway2&view=directions&Itemid=249'), JText::_('COM_RAILWAY2_DIRECTIONS'));?>
-		</li>
-		<li>
-			<?php echo JHtml::link(JRoute::_('index.php?option=com_content&view=article&id=62&Itemid=250'), 'Самые часто задаваемые вопросы');?>
-		</li>
-		<li>
-			<?php echo JHtml::link(JRoute::_('index.php?option=com_content&view=category&layout=blog&id=66&Itemid=251'), 'Терминология');?>
-		</li>
+		<?php
+        $menu = JFactory::getApplication()->getMenu()->getMenu();
+        foreach ($menu as $item) {
+            if ($item->parent_id == '248') {
+                ?>
+                <li>
+                    <?php echo JHtml::link(JRoute::_($item->link.'&Itemid='.$item->id), $item->title);?>
+                </li>
+                <?php
+            }
+        }
+        ?>
 	</ul>
 </div>
