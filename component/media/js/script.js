04129bbd36d6c350f3aa3b56208d800acfa74406
xakepok <xakepok@xakepok.com>
2018-04-19 14:32:25 +0300

    Увеличен шрифт в расписании

--- component/media/js/script.js
+++ component/media/js/script.js
@@ -35,3 +35,10 @@ function parseUrlQuery() {
     }
     return data;
 }
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+(function () {
+    var l = document.querySelectorAll('.rasp-table-th');
+    var ths = [].concat(_toConsumableArray(l));
+    console.log(ths.innerText);
+})();
