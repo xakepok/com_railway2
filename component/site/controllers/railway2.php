2c431fdef4b0ee1238027b1eb1fb15325fe4ccec
asharikov <xakepok@xakepok.com>
2018-06-16 11:42:23 +0300

    api

--- component/site/controllers/railway2.php
+++ component/site/controllers/railway2.php
@@ -23,4 +23,28 @@ class Railway2ControllerRailway2 extends JControllerLegacy
         $this->redirect();
         jexit();
     }
+
+    public function mobile()
+    {
+        $model = BaseDatabaseModel::getInstance('Mobile', 'Railway2Model', array());
+        $result = $model->getMobile();
+        $result = json_encode($result, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        jexit($result);
+    }
+
+    public function desc()
+    {
+        $model = BaseDatabaseModel::getInstance('Mobile', 'Railway2Model', array());
+        $result = $model->getDescTime();
+        $result = json_encode($result, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        jexit($result);
+    }
+
+    public function food()
+    {
+        $model = BaseDatabaseModel::getInstance('Station', 'Railway2Model', array());
+        $result = $model->getFood();
+        $result = json_encode($result, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        jexit($result);
+    }
 }
